3123ca54bbb1cc3701c7217f9f9a14c19ebe08a4
nelson <ludonelvic@gmail.com>
2023-07-09 15:49:51 +0100

    first structure

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import styled from 'styled-components';
-import { useNavigate } from 'react-router-dom';
-import { useAuthContext } from '../contexts/AuthContext';
-import axios from '../axios/instance';
+import React from "react";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import { useAuthContext } from "../contexts/AuthContext";
+import axios from "../axios/instance";
 
 const Home = () => {
   const { user, dispatch } = useAuthContext();
@@ -14,20 +14,172 @@ const Home = () => {
       {},
       {
         headers: {
-          Authorization: 'Bearer ' + user.accessToken,
+          Authorization: "Bearer " + user.accessToken,
         },
       }
     );
-    localStorage.removeItem('info');
-    dispatch({ type: 'UNSET_USER' });
-    navigate('/login');
+    localStorage.removeItem("info");
+    dispatch({ type: "UNSET_USER" });
+    navigate("/login");
   };
 
   return (
     <Wrapper>
       <h1>Home page</h1>
-      {user && user.email ? <h2>{user.email}</h2> : <h2>Not logged in</h2>}
-      {user && user.email && <button onClick={logout}>Logout</button>}
+      <div className="header">
+        <div>
+          <span>follow us on</span>
+          <img alt="tweeter Image" />
+          <img alt="facebook Image" />
+          <img alt="instagram Image" />
+          <img alt="tiktok Image" />
+          <button>contact us</button>
+          <a src="Login.js" className="login">
+            Login
+          </a>
+        </div>
+        <div>
+          <img alt="logo" />
+          <input type="text" value={"search drug here"} />
+          <img alt="seach Image" />
+          <select name="categories"></select>
+          <select name="AboutUs"></select>
+          <select name="OurSevices"></select>
+        </div>
+      </div>
+      <div className="headBoard">
+        <span>Manage your health and family with happiness</span>
+        <span>
+          Do you want to kick out sickness and maintain good health and fitness?
+        </span>
+        <span>sign up and get access to pharmaceutical drugs</span>
+        <a src="signIn.js" className="signIn">
+          sign in
+        </a>
+      </div>
+      <div className="allCategoriesOfDrugs">
+        <div className="categoryLine">
+          <div className="categoryContainer">
+            <div className="subCategoryContainer1">
+              <img alt="a category" />
+            </div>
+            <div className="subCategoryContainer2">
+              <span className="categoryName">stimulants</span>
+              <span className="categoryDescription">long borring text</span>
+            </div>
+          </div>
+          <div className="categoryContainer">
+            <div className="subCategoryContainer1">
+              <img alt="a category" />
+            </div>
+            <div className="subCategoryContainer2">
+              <span className="categoryName">stimulants</span>
+              <span className="categoryDescription">long borring text</span>
+            </div>
+          </div>
+          <div className="categoryContainer">
+            <div className="subCategoryContainer1">
+              <img alt="a category" />
+            </div>
+            <div className="subCategoryContainer2">
+              <span className="categoryName">stimulants</span>
+              <span className="categoryDescription">long borring text</span>
+            </div>
+          </div>
+        </div>
+        <div className="categoryLine">
+          <div className="categoryContainer">
+            <div className="subCategoryContainer1">
+              <img alt="a category" />
+            </div>
+            <div className="subCategoryContainer2">
+              <span className="categoryName">stimulants</span>
+              <span className="categoryDescription">long borring text</span>
+            </div>
+          </div>
+          <div className="categoryContainer">
+            <div className="subCategoryContainer1">
+              <img alt="a category" />
+            </div>
+            <div className="subCategoryContainer2">
+              <span className="categoryName">stimulants</span>
+              <span className="categoryDescription">long borring text</span>
+            </div>
+          </div>
+          <div className="categoryContainer">
+            <div className="subCategoryContainer1">
+              <img alt="a category" />
+            </div>
+            <div className="subCategoryContainer2">
+              <span className="categoryName">stimulants</span>
+              <span className="categoryDescription">long borring text</span>
+            </div>
+          </div>
+        </div>
+        <div className="categoryLine">
+          <div className="categoryContainer">
+            <div className="subCategoryContainer1">
+              <img alt="a category" />
+            </div>
+            <div className="subCategoryContainer2">
+              <span className="categoryName">stimulants</span>
+              <span className="categoryDescription">long borring text</span>
+            </div>
+          </div>
+          <div className="categoryContainer">
+            <div className="subCategoryContainer1">
+              <img alt="a category" />
+            </div>
+            <div className="subCategoryContainer2">
+              <span className="categoryName">stimulants</span>
+              <span className="categoryDescription">long borring text</span>
+            </div>
+          </div>
+          <div className="categoryContainer">
+            <div className="subCategoryContainer1">
+              <img alt="a category" />
+            </div>
+            <div className="subCategoryContainer2">
+              <span className="categoryName">stimulants</span>
+              <span className="categoryDescription">long borring text</span>
+            </div>
+          </div>
+        </div>
+        <div className="categoryLine">
+          <div className="categoryContainer">
+            <div className="subCategoryContainer1">
+              <img alt="a category" />
+            </div>
+            <div className="subCategoryContainer2">
+              <span className="categoryName">stimulants</span>
+              <span className="categoryDescription">long borring text</span>
+            </div>
+          </div>
+          <div className="categoryContainer">
+            <div className="subCategoryContainer1">
+              <img alt="a category" />
+            </div>
+            <div className="subCategoryContainer2">
+              <span className="categoryName">stimulants</span>
+              <span className="categoryDescription">long borring text</span>
+            </div>
+          </div>
+          <div className="categoryContainer">
+            <div className="subCategoryContainer1">
+              <img alt="a category" />
+            </div>
+            <div className="subCategoryContainer2">
+              <span className="categoryName">stimulants</span>
+              <span className="categoryDescription">long borring text</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="footer">
+        <span> copyright &copy; pharmacy app</span>
+      </div>
+      {/* {user && user.email ? <h2>{user.email}</h2> : <h2>Not logged in</h2>}
+      {user && user.email && <button onClick={logout}>Logout</button>} */}
     </Wrapper>
   );
 };
